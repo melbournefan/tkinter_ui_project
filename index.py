c977a109ba38c101ba520a793dcc97b1f2dbff50
Matthew Davalle <41057813+melbournefan@users.noreply.github.com>
2025-10-07 15:45:27 +1100

    created window

--- index.py
+++ index.py
@@ -1,3 +1,9 @@
 import tkinter as tk
 window = tk.Tk()
 window.mainloop()
+class   Application(tk.Frame):
+    def __init__(self, master=None):
+        super(). init (master)
+        master = tk.Tk()
+        app = Application(master=master)
+        app.mainloop()
